762981106977ddd92f0fca1c1974a9f6fc1cf75d
Nashtech <naincygupta100@gmail.com>
2025-03-06 16:05:27 +0530

    add more tests

--- sum.test.js
+++ sum.test.js
@@ -13,12 +13,4 @@ test('adds 0 + 0 to equal 0', () => {
   expect(sum(0, 0)).toBe(0); // Test if sum(0, 0) equals 0
 });
 
-// Test for adding 2 + 3
-test('adds 2 + 3 to equal 5', () => {
-  expect(sum(2, 3)).toBe(5);
-});
 
-// Test for adding -2 + 3
-test('adds -2 + 3 to equal 1', () => {
-  expect(sum(-2, 3)).toBe(1);
-});
